557ce4e49342db37b115995b6eb33aeedebb9dcc
Felipe Cembranelli <felipecembranelli@live.com>
2025-01-03 16:06:47 -0800

    simulating failure 5

--- Services/ChannelService.cs
+++ Services/ChannelService.cs
@@ -29,7 +29,7 @@ namespace OpenAiVideoSummary.Api.Service
         /// <returns>A task that represents the asynchronous operation. The task result contains a list of channels.</returns>
         public async Task<List<Channel>> GetAllChannelsAsync()
         {
-            // changed test4
+            // changed test5
 
             //return await _channelRepository.GetAllAsync();
             return null;
